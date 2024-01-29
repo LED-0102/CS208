d35d81a7e9cda12007f6151c7a2ae729170e58ac
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-01-29 22:43:00 +0530

    Modal

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <div>
+      <LandingPage></LandingPage>
+     
     </div>
   );
 }
---
+++ FrontEnd/client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+import ModalComponent from "../../components/modal/Modal.js";
+
+
+export default function LandingPage(){
+return(
+    <div>
+        <ModalComponent></ModalComponent>
+    </div>
+);
+}
