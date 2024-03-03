e770967c6fddeb50ee94161f5dfb962f7d15c78c
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-03-03 23:23:30 +0530

    Routes

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage/LandingPage';
 import FacultyHome from './pages/HomePages/FacultyHome/FacultyHome';
 
 function App() {
   return (
-    <div>
-      {/* <LandingPage></LandingPage> */}
-      <FacultyHome></FacultyHome>
-    </div>
+    // <div>
+    //   {/* <LandingPage></LandingPage> */}
+    //   <FacultyHome></FacultyHome>
+    // </div>
+
+    <>
+       <Routes>
+       <Route path="/LandingPage" element={<LandingPage/>}></Route>
+ 
+       <Route path="/FacultyHome" element={<FacultyHome/>}></Route>
+ 
+        </Routes>
+
+    </>
   );
 }
 
