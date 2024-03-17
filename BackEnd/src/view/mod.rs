e0ed60bbbd2d373f1e07c81e0a0a6c97d32dbbeb
Akhilesh P <panchangamakhil@gmail.com>
2024-03-17 23:33:06 +0530

    Accepting and Rejecting SS04 back-end logic done.

--- BackEnd/src/view/mod.rs
+++ BackEnd/src/view/mod.rs
@@ -2,8 +2,9 @@ mod forms;
 
 use std::error::Error;
 use std::str::FromStr;
-use actix_web::{HttpResponse, post, web};
+use actix_web::{post, web::{self, Data}, HttpResponse};
 use serde_json::Value;
+use sqlx::query;
 use crate::auth::jwt::JwToken;
 use forms::{Forms, FormTrait};
 use crate::AppState;
@@ -17,7 +18,73 @@ pub fn view_config (cfg: &mut web::ServiceConfig) {
     cfg.service(
         web::scope("/v1")
             .service(form_handler)
+            .service(accept_ss04)
+            .service(reject_ss04)
     );
+}
+
+#[post("/ss04_accept/{user_id}/{primary_key}")]
+pub async fn accept_ss04(app_state: Data<AppState>, path: web::Path<(i32, i32)>) -> HttpResponse{
+    let (user_id, primary_key) = path.into_inner();
+    match sqlx::query("
+        UPDATE SS04 SET hod_approval = 'Accepted' WHERE id = &1;
+        UPDATE users
+        SET seeking = jsonb_set(seeking, '{SS04}', ((seeking -> 'SS04') #- \"{$2}\"))
+        WHERE id = $3;
+        UPDATE users
+        SET previous = 
+            CASE
+                WHEN seeking ? 'SS04' THEN
+                    jsonb_set(seeking, '{SS04}', seeking->'SS04' || '$4'::jsonb)
+                ELSE
+                    '{\"SS04\" : [&5]}'::jsonb
+            END
+        WHERE id = &6;
+    ")
+        .bind(primary_key)
+        .bind(primary_key)
+        .bind(user_id)
+        .bind(primary_key)
+        .bind(primary_key)
+        .bind(user_id)
+        .execute(&app_state.pool)
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().body("Error accepting SS04 from")
+    }
+}
+
+#[post("/ss04_reject/{user_id}/{primary_key}")]
+pub async fn reject_ss04(app_state: Data<AppState>, path: web::Path<(i32, i32)>) -> HttpResponse{
+    let (user_id, primary_key) = path.into_inner();
+    match sqlx::query("
+        UPDATE SS04 SET hod_approval = 'Rejected' WHERE id = &1;
+        UPDATE users
+        SET seeking = jsonb_set(seeking, '{SS04}', ((seeking -> 'SS04') #- \"{$2}\"))
+        WHERE id = $3;
+        UPDATE users
+        SET previous = 
+            CASE
+                WHEN seeking ? 'SS04' THEN
+                    jsonb_set(seeking, '{SS04}', seeking->'SS04' || '$4'::jsonb)
+                ELSE
+                    '{\"SS04\" : [&5]}'::jsonb
+            END
+        WHERE id = &6;
+    ")
+        .bind(primary_key)
+        .bind(primary_key)
+        .bind(user_id)
+        .bind(primary_key)
+        .bind(primary_key)
+        .bind(user_id)
+        .execute(&app_state.pool)
+        .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(_) => HttpResponse::InternalServerError().body("Error accepting SS04 from")
+    }
 }
 
 #[post("/submit/{form_type}")]
