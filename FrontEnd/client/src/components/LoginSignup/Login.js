66b1e93c5fd9a81ee2ca04a84046fb4b6c3af10b
Vedant4581 <vedant4947@gmail.com>
2024-03-20 18:26:56 +0530

    commit to main

--- FrontEnd/client/src/components/LoginSignup/Login.js
+++ FrontEnd/client/src/components/LoginSignup/Login.js
@@ -6,6 +6,8 @@ import google_icon from '../../images/LoginSignup/google.svg'
 import globalUrl from "../url";
 import Cookies from 'js-cookie';
 import axios from "axios";
+import { Link } from 'react-router-dom'
+import "./login.css"
 
 
 const LoginSignup = () => {
@@ -60,9 +62,7 @@ const LoginSignup = () => {
     };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div
-        className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0"
       >
         {/* <!-- left side --> */}
         <div className="flex flex-col justify-center p-8 md:p-14">
@@ -75,7 +75,6 @@ const LoginSignup = () => {
           <div className="py-4">
             <span className="mb-2 text-md">Email</span>
             <input
-              type="text"
               className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
               name="email"
               id="email"
@@ -95,14 +94,9 @@ const LoginSignup = () => {
             />
           </div>
           <div className="flex justify-between w-full py-4">
-            <div className="mr-24">
-              <input type="checkbox" name="ch" id="ch" className="mr-2" />
-              <span className="text-md">Remember for 30 days</span>
             </div>
-            <span className="font-bold text-md">Forgot password</span>
           </div>
           <button
-            className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300"
             type="submit"
             onClick={(e) => handleSubmit(e)}
           >
@@ -117,21 +111,17 @@ const LoginSignup = () => {
           </button>
           <div className="text-center text-gray-400">
             Dont'have an account?
-            <span className="font-bold text-black">Sign up for free</span>
           </div>
         </div>
         {/* right side */}
         <div className="relative">
           <img
-            src=""
             alt="img"
             className="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"
           />
           {/* <!-- text on image  --> */}
           <div
-            className="absolute hidden bottom-10 right-6 p-6 bg-white bg-opacity-30 backdrop-blur-sm rounded drop-shadow-lg md:block"
           >
-            <span className="text-white text-xl"
               >We've been uesing Untitle to kick"<br />start every new project
               and can't <br />imagine working without it."
             </span>
