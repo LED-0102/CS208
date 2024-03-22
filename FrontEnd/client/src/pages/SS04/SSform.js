4ba7c5f04c40abc2e0c6babefe3306caf93daf4d
LED-0102 <darshilamit@gmail.com>
2024-03-22 16:11:40 +0530

    bug fixes

--- FrontEnd/client/src/pages/SS04/SSform.js
+++ FrontEnd/client/src/pages/SS04/SSform.js
@@ -136,16 +136,25 @@ const SS04form = () => {
     console.log("update form data:", updatedFormData)
 
     try {
-      const token = localStorage.getItem('jwt');
-      const response = await axios.post(`${globalUrl}/v1/submit/SS04`, updatedFormData, {
-        headers: {
-          token: token
-        }
-          }
-      );
-      // const { id } = response.data;
-      // navigate(`/SS04/${id}`);
-      console.log("Response from server:", response.data);
+
+      const storedCookie = document.cookie;
+      console.log(storedCookie);
+// Create a custom set of headers
+      const customHeaders = new Headers({
+        'Content-Type': 'application/json', // You may need to adjust the content type based on your request
+        'Cookie': storedCookie, // Include the retrieved cookie in the 'Cookie' header
+      });
+      const headersObject = Object.fromEntries(customHeaders.entries());
+      // const response = await fetch('https://jsonplaceholder.typicode.com/posts',{
+      const response = await fetch('${globalUrl}/v1/submit/SS04', {
+        method: 'POST',
+        credentials: 'include',  // Include credentials (cookies) in the request
+        headers: headersObject,
+        body: JSON.stringify(updatedFormData)
+      });
+      if (response.statusCode === 401) {
+        console.log("Failed");
+      }
     } catch (error) {
       console.error("Error:", error);
     }
