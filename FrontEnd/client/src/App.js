d3c6e921ec0a8b4ff653b23775491f99e0a6ff07
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-03-31 18:26:16 +0530

    routes

--- FrontEnd/client/src/App.js
+++ FrontEnd/client/src/App.js
@@ -16,6 +16,7 @@ import Home from './pages/HomePages/Home';
 import E01 from './pages/E01/E01';
 import R1 from './pages/R1/R1';
 import DisplayPendingForm from './pages/DisplayPendingForm/DisplayPendingForm';
+import SS04SpecificDisplayPendingForm from './pages/DisplayPendingForm/SS04SpecificDisplayPendingForm'
 
 
 function App() {
@@ -44,6 +45,7 @@ function App() {
           <Route exact path="/e01" element={ <E01 /> } />
           <Route exact path="/r1" element={ <R1 /> } />
           <Route exact path="/displayPendingForm" element={ <DisplayPendingForm /> } />
+          <Route exact path="/displayPendingForm/SS04/:id" element={ <SS04SpecificDisplayPendingForm /> } />
 
           <Route exact path="/availableForms" element={ <Selectform /> } />
   
