15053099ac8e90b2f4984adf2bccef34b7073592
Parth Deshmukh <deshmukhparth293@gmail.com>
2024-04-12 19:24:36 +0530

    Bugs

--- FrontEnd/client/src/pages/DisplaySeekingForm/SpecificDisplaySeekingForm.js
+++ FrontEnd/client/src/pages/DisplaySeekingForm/SpecificDisplaySeekingForm.js
@@ -547,70 +547,7 @@ console.log("formData++++",formData)
 
             </div>
           </div>
-          <div className='p-4'> 
-          <div>
-         <div className="flex flex-col lg:flex-row mb-4 lg:mb-8 font-custom">
-        <div className="mb-4 lg:mb-0 lg:mr-4 lg:w-full">
-          <input
-            type="text"
-            placeholder="Search by name"
-            value={searchName}
-            onChange={(e) => setSearchName(e.target.value)}
-            className="p-2 border w-full rounded-md search-input hover:bg-gray-200"
-          />
-        </div>
-        <div className="mb-4 lg:mb-0 lg:mr-4 lg:w-full">
-          <select
-            value={selectedDesignation}
-            onChange={(e) => setSelectedDesignation(e.target.value)}
-            className="p-2 border w-full rounded-md text-white"
-            style={{ backgroundColor: "rgb(30 41 59)" }}
-          >
-            <option value="">Select Department</option>
-            {designation.map((department) => (
-              <option key={department} value={department}>
-                {department}
-              </option>
-            ))}
-          </select>
-        </div>
-        </div>
-        {searchName.toLowerCase() !== '' && (
-        <table className="w-full lg:w-full table-auto  border-collapse font-custom">
-        <thead>
-          <tr>
-            <th className="w-1/3 border-4 p-2 text-center font-bold text-purple-900">
-              Name
-            </th>
-            <th className="w-1/3 border-4 p-2 text-center font-bold text-purple-900">
-              Designation
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filterUsers().map((user, index) => (
-            <tr
-              key={user.id}
-              className="bg-slate-950 hover:bg-slate-800 transition-all cursor-pointer"
-              onClick={() => handleUserSelect(user.id,user.username)}>
-
-              <td className="w-1/3 border-4 p-4 bg-white subpixel-antialiased text-teal-500 ">
-                {user.username}
-              </td>
-              <td className="w-1/3 border-4 p-4 bg-white text-center text-cyan-500">
-                {user.designation}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-        )}
-        
-        {/* <p className="text-black">hwlooooooooooooooooooooooooooooooooooooooooooo</p> */}
-    </div>
-
-           
-            </div>
+         
           {/* <div className='flex justify-center w-full mb-8'>
             <button onClick={(e) => handleSubmit(e)} >Submit</button>
           </div> */}
@@ -1220,70 +1157,7 @@ const SS01form = () => {
                               </div>
                           </div>
 
-                          <div className='p-4'>
-                              <div>
-                                  <div className="flex flex-col lg:flex-row mb-4 lg:mb-8 font-custom">
-                                      <div className="mb-4 lg:mb-0 lg:mr-4 lg:w-full">
-                                          <input
-                                              type="text"
-                                              placeholder="Search by name"
-                                              value={searchName}
-                                              onChange={(e) => setSearchName(e.target.value)}
-                                              className="p-2 border w-full rounded-md search-input hover:bg-gray-200"
-                                          />
-                                      </div>
-                                      <div className="mb-4 lg:mb-0 lg:mr-4 lg:w-full">
-                                          <select
-                                              value={selectedDesignation}
-                                              onChange={(e) => setSelectedDesignation(e.target.value)}
-                                              className="p-2 border w-full rounded-md text-white"
-                                              style={{ backgroundColor: "rgb(30 41 59)" }}
-                                          >
-                                              <option value="">Select Department</option>
-                                              {designation.map((department) => (
-                                                  <option key={department} value={department}>
-                                                      {department}
-                                                  </option>
-                                              ))}
-                                          </select>
-                                      </div>
-                                  </div>
-                                  {searchName.toLowerCase() !== '' && (
-                                      <table className="w-full lg:w-full table-auto  border-collapse font-custom">
-                                          <thead>
-                                              <tr>
-                                                  <th className="w-1/3 border-4 p-2 text-center font-bold text-purple-900">
-                                                      Name
-                                                  </th>
-                                                  <th className="w-1/3 border-4 p-2 text-center font-bold text-purple-900">
-                                                      Designation
-                                                  </th>
-                                              </tr>
-                                          </thead>
-                                          <tbody>
-                                              {filterUsers().map((user, index) => (
-                                                  <tr
-                                                      key={user.id}
-                                                      className="bg-slate-950 hover:bg-slate-800 transition-all cursor-pointer"
-                                                      onClick={() => handleUserSelect(user.id, user.username)}>
-
-                                                      <td className="w-1/3 border-4 p-4 bg-white subpixel-antialiased text-teal-500 ">
-                                                          {user.username}
-                                                      </td>
-                                                      <td className="w-1/3 border-4 p-4 bg-white text-center text-cyan-500">
-                                                          {user.designation}
-                                                      </td>
-                                                  </tr>
-                                              ))}
-                                          </tbody>
-                                      </table>
-                                  )}
-
-
-                              </div>
-
-
-                          </div>
+                    
                           <div></div>
                           
 
